30ee396e311e683fc6c5dd7eca90223f2dd4e4f7
Seokjun Hong <tmax2017292@gmail.com>
2017-08-30 17:38:38 +0900

    add Stest
    CVT test

---
+++ Stest/CVT01/COPXOR01.asm
@@ -0,0 +1,32 @@
+COPXOR      CSECT
+            LR        12,15
+            USING     PSA,0
+            L         7,FLCCVT
+            USING     CVT,7
+            L         6,PSATOLD
+            USING     TCB,6
+            L         6,TCBEXT2
+*            DROP      6
+            USING     TCBXTNT2,6
+            L         0,TCBCAUF
+*            DROP      6
+            L         6,PSAAOLD
+            USING     ASCB,6
+            L         6,ASCBASXB
+*            DROP      6
+            USING     ASXB,6
+            OFADBGMEM ASXBUSER,0
+
+            BR        14
+            EJECT
+            IHAPSA    DSECT=YES,LIST=YES
+            EJECT
+            CVT       DSECT=YES,LIST=YES
+            EJECT
+            IKJTCB    DSECT=YES,LIST=YES
+            EJECT
+            IHAASCB   DSECT=YES,LIST=YES
+            EJECT
+            IHAASXB   DSECT=YES,LIST=YES
+            END
+            
